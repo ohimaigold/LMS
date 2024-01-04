70b5cf1736f90f2181638fea805d3c3f10d16c0b
ohimaigold <ohimaigold@gmail.com>
2024-01-04 11:07:51 +0000

    changed about us

--- frontend/src/CLIENT/about/AboutUsBanner.jsx
+++ frontend/src/CLIENT/about/AboutUsBanner.jsx
@@ -7,8 +7,8 @@ const AboutUsBanner = () => {
     <div className='about-div-with-background border mt-5'>
       <Container>
         <Row className='about-quote-container mt-5 me-1'>
-          <h1>"Don't settle down and sit in one place. Move around, be nomadic, make each day a new horizon."</h1>
-          <p>― Jon Krakauer, Into the Wild.</p>
+          <h1>"When you want something in life, you just gotta reach out and grab it."</h1>
+          <p>― Christopher McCandless.</p>
         </Row>
       </Container>
     </div>
